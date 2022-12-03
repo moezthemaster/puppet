b10b33d1db8cf08e5a6d123978a46dcdd4752fc1
moezthemaster <moezthemaster@hotmail.fr>
2022-12-03 23:49:07 +0100

    Update run-puppet.sh

--- files/run-puppet.sh
+++ files/run-puppet.sh
@@ -1,2 +1,3 @@
-cd /etc/puppetlabs/code/environments/production && git pull
-/opt/puppetlabs/bin/puppet apply manifests/
+#!/bin/bash
+
+cd /etc/puppetlabs/code/environments/production && git pull && /opt/puppetlabs/bin/puppet apply manifests/
